b8e8c235c4a07274b5169314e68fd5e8409762eb
cpeninou <cyrille.peninou@nestle.com>
2016-05-25 16:28:28 +0200

    Add 2 class et un pom

---
+++ src/main/java/com/perso/Main.java
@@ -0,0 +1,10 @@
+package com.perso;
+
+/**
+ * Created by cpenino on 25/05/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        MasterObject mobj = new MasterObject();
+    }
+}
---
+++ src/main/java/com/perso/MasterObject.java
@@ -0,0 +1,7 @@
+package com.perso;
+
+/**
+ * Created by cpenino on 25/05/16.
+ */
+public class MasterObject {
+}
